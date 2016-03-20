75f1c57e0cd4ac4f4f847fb308ba22e0212af820
Uğur Oruç <uoruc5@hotmail.com>
2016-03-20 13:55:34 +0200

    Spotify Login Add and test on tablet

--- client/templates/side/side.js
+++ client/templates/side/side.js
@@ -4,4 +4,25 @@ Template.side.onRendered(function () {
     edge:'left',
     closeOnClick:false
   });
+  $(".login-spotify").on('click',function(){
+    var options = {
+      showDialog: true, // Whether or not to force the user to approve the app again if they’ve already done so.
+      requestPermissions: ['user-read-email'] // Spotify access scopes.
+    };
+    Meteor.loginWithSpotify(options, function(err) {
+      console.log(err || "Success Login");
+    });
+
+  });
+  $(".logout").on('click',function(){
+    Meteor.logout();
+  });
+});
+Template.side.helpers({
+  profilePicture:function(){
+    return Meteor.user().profile.images[0].url;
+  },
+  username:function(){
+    return Meteor.user().profile.display_name;
+  }
 });
--- server/methods.js
+++ server/methods.js
@@ -9,35 +9,5 @@ ServiceConfiguration.configurations.update(
   { upsert: true }
 );
 Meteor.methods({
-  refreshAccess : function(access){
-    var clientId = "fe902a4695f945639d3d0f162586d971";
-    var url = "https://accounts.spotify.com/api/token"
-    var token = HTTP.post(url,{
-      params:{
-        access_token: access,
-        token_type:'Bearer',
-        expires_in:'36000',
-        refresh_token:access,
-      },
-    },function(error,response){
-      console.log(error);
-      console.log(response)
-    });
-
-  },
-  me:function(access){
-    var url = 'https://api.spotify.com/v1/me';
-    var me = HTTP.get(url,{
-      headers:{
-        'Authorization':' Bearer '+access,
-      },
-    },function(error,response){
-      if(error){
-        console.log(error);
-      }
-      else {
-        console.log(response);
-      }
-    });
-  }
+  
 });
